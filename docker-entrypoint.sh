4a32ff4b3816c8c603b787f6df7e06784a91adbf
Anthony Oteri <anthony.oteri@edgeware.tv>
2021-11-16 19:37:35 -0500

    Create user/admin user automatically in docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,12 @@
 python3 /app/manage.py migrate --no-input
 python3 /app/manage.py collectstatic
 
+if [ ! -z "${HYDRA_ROOT_USERNAME}" -a ! -z "${HYDRA_ROOT_PASSWORD}" ]; then
+    python3 /app/manage.py createuser --username "${HYDRA_ROOT_USERNAME}" --password "${HYDRA_ROOT_PASSWORD}" --super --ignore-existing
+fi
+
+if [ ! -z "${HYDRA_USERNAME}" -a ! -z "${HYDRA_PASSWORD}" ]; then
+    python3 /app/manage.py createuser --username "${HYDRA_USERNAME}" --password "${HYDRA_PASSWORD}" --ignore-existing
+fi
+
 exec $@
